ccc7d94df98712e6e1af2f9fa697bb4196c1bcce
Torrey Atcitty <torrey@compound.finance>
2021-01-25 16:20:33 -0800

    Fix missing imports used when obtaining ledger accounts.

--- src/js/sharedJs/math.js
+++ src/js/sharedJs/math.js
@@ -1,45 +1,39 @@
-import BN from "../../../node_modules/bn.js";
+import BN from 'bn.js';
 
 function toScaledDecimal(bn, decimal) {
-    return {
-        man: bn.toString(),
-        exp: Number(decimal),
-    };
+  return {
+    man: bn.toString(),
+    exp: Number(decimal),
+  };
 }
 
 function realToBigNumberScaled(real, scale) {
-    // TODO: This is a hack since BN.js won't let us do proper float division :(
-    if (real < 1) {
-        return new BN(10).pow(new BN(scale)).div(new BN(1 / real));
-    } else {
-        return new BN(10).pow(new BN(scale)).mul(new BN(real));
-    }
+  // TODO: This is a hack since BN.js won't let us do proper float division :(
+  if (real < 1) {
+    return new BN(10).pow(new BN(scale)).div(new BN(1 / real));
+  } else {
+    return new BN(10).pow(new BN(scale)).mul(new BN(real));
+  }
 }
 
 function parseWeiStr(weiStr) {
-    return new BN(weiStr);
+  return new BN(weiStr);
 }
 
 function resultToNumber(result) {
-    if (result) {
-        if (Number.isInteger(result)) {
-            return result;
-        } else {
-            return Number(result);
-        }
+  if (result) {
+    if (Number.isInteger(result)) {
+      return result;
     } else {
-        return null;
+      return Number(result);
     }
+  } else {
+    return null;
+  }
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-export {
-    getRandomInt,
-    parseWeiStr,
-    realToBigNumberScaled,
-    resultToNumber,
-    toScaledDecimal,
-};
+export { getRandomInt, parseWeiStr, realToBigNumberScaled, resultToNumber, toScaledDecimal };
